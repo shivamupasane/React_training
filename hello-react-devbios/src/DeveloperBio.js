98a985c33a0052c9710873766d51cb462e931db1
shivamupasane <shivamupasane@yahoo.com>
2021-05-04 20:37:38 +0530

    react initial setup and functional component

---
+++ hello-react-devbios/src/DeveloperBio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function DeveloperBio() {
+    return (
+        <div className="card d-inline-block text-dark">
+            <div className="card-body">
+                <h5 className="card-title">Shivam Upasane's Bio</h5>
+                <p className="card-text">
+                    <span className="font-weight-bold">Favorite Language:</span> Javascript
+                </p>
+                <p className="card-text">
+                    <span className="font-weight-bold">Year Started:</span> 2017
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default DeveloperBio
